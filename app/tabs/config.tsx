33b60c1150e79745df36c6af7c8ca9596e1a1346
maryson gleison <maryson011@gmail.com>
2024-07-13 17:51:35 -0300

    feat - menu de navegação

---
+++ app/tabs/config.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "../../constants/styles";
+
+export default function TabConfig() {
+    return (
+        <View style={styles.centralizado}>
+            <Text>Tab de Config</Text>
+        </View>
+    )
+}
---
+++ app/tabs/cursos.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "../../constants/styles";
+
+export default function TabCursos() {
+    return (
+        <View style={styles.centralizado}>
+            <Text>Tab de Cursos</Text>
+        </View>
+    )
+}
